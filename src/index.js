156ec366b9ee416943c8354a8768b1b874867d8a
elaizahoxha <elaizahoxha@gmail.com>
2023-07-26 12:32:13 -0700

    Updated the temperature for the forecast

--- src/index.js
+++ src/index.js
@@ -21,27 +21,45 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
   }
 
+  function formatDay (timestamp){
+
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days =  [
+      "Sun",
+      "Mon",
+      "Tue",
+      "Wed",
+      "Thu",
+      "Fri",
+      "Sat"]
+
+    return days[day];
+
+  }
+
   function displayForecast(response){
 
-console.log(response.data);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
     
     let forecastHTML = `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-    days.forEach(function(day) {
+    forecast.forEach(function(forecastDay, index) {
+      if (index < 6) {
       forecastHTML = forecastHTML + 
       `
       <div class="col-2">
           <div class="week-day">
-          ${day}
+          ${formatDay(forecastDay.dt)}
       </div>
-          <img src="http://openweathermap.org/img/wn/01n@2x.png" alt="" width="36"> 
+          <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36"> 
           <div class="weather-temperature-forcast">
-       <span class="weather-temperature-forcast-high">18°</span> 
-       <span class="weather-temperature-forcast-low">12°</span> 
+       <span class="weather-temperature-forcast-high"> ${Math.round(forecastDay.temp.max)}°</span> 
+       <span class="weather-temperature-forcast-low"> ${Math.round(forecastDay.temp.min)}°</span> 
       </div>
       </div>
   `;  
+    }
     })
        
 forecastHTML = forecastHTML + `</div>`
